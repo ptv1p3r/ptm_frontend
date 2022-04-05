fef3785313a40fd442ca9cd9b31606a4c8a034fb
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-04-05 21:24:38 +0100

    Atualizei os CND's do footer e do header do frontend

--- Views/_includes/header.php
+++ Views/_includes/header.php
@@ -15,17 +15,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> 
+
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
     <script src="<?php echo auto_version('../../js/global-functions.js'); ?>"></script>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+
     <link rel="stylesheet" href="<?php echo auto_version('../../css/movie.css'); ?>">
--->
     <link rel="icon" href="Images/home/favicon.png">
+
+    <!--   CSS que vinham com o template
 
     <link href="css/home/custom.css" rel="stylesheet">
     <link href="css/home/color.css" rel="stylesheet">
@@ -34,7 +38,7 @@
     <link href="css/home/bootstrap.min.css" rel="stylesheet">
     <link href="css/home/prettyPhoto.css" rel="stylesheet">
     <link href="css/home/all.min.css" rel="stylesheet">
-
+   -->
 
     <title><?php echo $this->title?></title>
 
--- Views/home/home-view.php
+++ Views/home/home-view.php
@@ -30,7 +30,7 @@
                     <a href="<?php // echo HOME_URI . '/detail/view/' . $movie["movid"];?>" class="btn btn-success">Details</a>
                 </div>
             </div>
-        <?php// } ?>
+         <?php //} ?>
     </div>
 </div>
  Top Downloaded
@@ -48,7 +48,7 @@
                     <a href="<?php //echo HOME_URI . '/detail/view/' . $movie["movid"];?>" class="btn btn-success">Details</a>
                 </div>
             </div>
-        <?php// } ?>
+        <?php // } ?>
     </div>
 </div>
  Last Added
@@ -57,16 +57,16 @@
     <div class="row" style="margin-bottom: 10%;">
         <?php //foreach ($moviesLastAdded as $movie) { ?>
             <div class="card" style="margin:10px auto;width: 200px;height: 300px">
-                <a href="<?php// echo HOME_URI . '/detail/view/' . $movie["movid"];?>">
+                <a href="<?php // echo HOME_URI . '/detail/view/' . $movie["movid"];?>">
                     <img class="card-img-top" src="<?php //echo $movie["poster"]; ?>" alt="<?php //echo $movie["title"]; ?>">
                 </a>
                 <div class="card-body">
                     <h5 class="card-title text-white"><?php //echo $movie["title"]; ?></h5>
-                    <p class="card-text text-muted"><?php// echo $movie["year"]; ?></p>
+                    <p class="card-text text-muted"><?php // echo $movie["year"]; ?></p>
                     <a href="<?php //echo HOME_URI . '/detail/view/' . $movie["movid"];?>" class="btn btn-success">Details</a>
                 </div>
             </div>
-        <?php// } ?>
+        <?php // } ?>
     </div>
 </div>
 -->
@@ -1103,104 +1103,7 @@
     <!--InstaGram End-->
 
 
-    <!--Footer Start
-    <footer class="footer">
-        <div class="footer-top wf100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <!--Footer Widget Start
-                        <div class="footer-widget">
-                            <h4>About Ecova</h4>
-                            <p> If anything’s hot in today’s economy, it’s saving money, including a broad range of green businesses helping people save energy, water, and other resources. Definitely, you can go with this business as it is a nothing but the future. </p>
-                            <a href="#" class="lm">About us</a>
-                        </div>
-                        <!--Footer Widget End
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <!--Footer Widget Start
-                        <div class="footer-widget">
-                            <h4>Our Promises</h4>
-                            <ul class="quick-links">
-                                <li><a href="#">Solar Energy</a></li>
-                                <li><a href="#">Waste Management</a></li>
-                                <li><a href="#">Eco Ideas</a></li>
-                                <li><a href="#">Recycling Materials</a></li>
-                                <li><a href="#">Plant Ecology</a></li>
-                                <li><a href="#">Saving Wildlife </a></li>
-                            </ul>
-                        </div>
-                        <!--Footer Widget End
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <!--Footer Widget Start
-                        <div class="footer-widget">
-                            <h4>Latest Posts</h4>
-                            <ul class="lastest-products">
-                                <li> <img src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/images/flp1.jpg" alt=""> <strong><a href="#">How you can keep alive wild animals for...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
-                                <li> <img src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/images/flp2.jpg" alt=""> <strong><a href="#">Eliminate your plastic bottle pollution & keep...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
-                                <li> <img src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/images/flp3.jpg" alt=""> <strong><a href="#">How you can keep alive wild animals for...</a></strong> <span class="pdate"><i>Posted:</i> 29 September, 2018</span> </li>
-                            </ul>
-                        </div>
-                        <!--Footer Widget End
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <!--Footer Widget Start
-                        <div class="footer-widget">
-                            <div id="fpro-slider" class="owl-carousel owl-theme">
-                                <!--Footer Product Start
-                                <div class="item">
-                                    <div class="f-product">
-                                        <img src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/images/fpro1.jpg" alt="">
-                                        <div class="fp-text">
-                                            <h6><a href="#">Buy T-Shirts</a></h6>
-                                            <strong>$19.00</strong>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--Footer Product End-->
-                                <!--Footer Product Start
-                                <div class="item">
-                                    <div class="f-product">
-                                        <img src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/images/fpro1.jpg" alt="">
-                                        <div class="fp-text">
-                                            <h6><a href="#">Buy T-Shirts</a></h6>
-                                            <strong>$19.00</strong>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--Footer Product End-->
-                                <!--Footer Product Start
-                                <div class="item">
-                                    <div class="f-product">
-                                        <img src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/images/fpro1.jpg" alt="">
-                                        <div class="fp-text">
-                                            <h6><a href="#">Buy T-Shirts</a></h6>
-                                            <strong>$19.00</strong>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--Footer Product End
-                            </div>
-                        </div>
-                        <!--Footer Widget End
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyr wf100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-sm-4"> <img src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/images/logo.png" alt=""> </div>
-                    <div class="col-md-8 col-sm-8">
-                        <p> All Rights Reserved of Ecova © 2018, Design & Developed By: GramoTech </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!--Footer End-->
-</div>
+
 <!--   JS Files Start
 <script src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/js/jquery-3.3.1.min.js"></script>
 <script src="../../../../../Users/User/Desktop/Main%20Files/Main%20Files/js/jquery-migrate-1.4.1.min.js"></script>
