4ae5dc084dcc29fa8a93addcbf956cbc201797e5
AndMJ <andrejoao070@gmail.com>
2022-04-13 22:09:59 +0100

    ADD callAPI function for communication with backend

--- Includes/global-functions.php
+++ Includes/global-functions.php
@@ -67,3 +67,48 @@ function timeCalculation($time){
         return $numberOfUnits.' '.$text.(($numberOfUnits>1)?'s':'');
     }
 }
+
+// Metodo que efetua os pedidos a api e retorna os valores
+function callAPI($method, $url, $data, $token = ""){
+    $curl = curl_init();
+
+    switch ($method){
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+            break;
+        case "DELETE":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+            break;
+        case "GET":
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    // OPTIONS:
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'Authorization: ' . $token,
+        'Content-Type: application/json'
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if(!$result){die("Connection Failure");}
+    curl_close($curl);
+    return $result;
+}
