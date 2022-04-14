9d56d25e11522fd57eb75b91dce58b0352a429ed
AndMJ <andrejoao070@gmail.com>
2022-04-14 01:32:43 +0100

    ADD Update/Edit and Delete functions

--- Models/admin-model.php
+++ Models/admin-model.php
@@ -34,9 +34,10 @@ class AdminModel extends MainModel {
         return $query->fetchAll();
     }*/
 
+
+    /** CRUD GROUPS **/
     /**
      * Metodo que retorna Grupo pelo id
-     * @return array
      */
     public function getGroupById($id) {
         $result = null;
@@ -51,37 +52,116 @@ class AdminModel extends MainModel {
         return json_decode($result, true);
     }
 
-    /*public function setCategory($name){
-        $query = null;
+    /**
+     * Metodo adiciona Grupo
+     */
+    public function addGroup($data) {
+        $result = null;
+        $normalizedData = array();
 
-        $query = $this->db->query('SELECT Auto_increment as id FROM information_schema.tables 
-            WHERE table_name=\'categories\'');
+        // Not active by default
+        $normalizedData['Active'] = "";
 
-        $result = $query->fetch(PDO::FETCH_ASSOC);
+        // get data from form array and package it to send to api
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']) { //gets <input name="">
+                    case "addGroupName":
+                        $normalizedData['Name'] = $value;
+                        break;
 
-        $insert = 'INSERT INTO categories (catid, name) VALUES (' . $result["id"] . ', \'' . $name . '\')';
+                    case "addGroupDescription":
+                        $normalizedData['Description'] = $value;
+                        break;
 
-        $this->db->query($insert);
-    }*/
+                    case "addGroupSecurityId":
+                        $normalizedData['SecurityId'] = $value;
+                        break;
 
-    /* function updateCategory($id, $name){
-        $update = null;
+                    case "addGroupActive":
+                        $normalizedData['Active'] = "True";
+                        break;
+                }
+            }
+        }
 
-        $update = 'UPDATE categories SET name=\'' . $name . '\' WHERE catid = ' . $id ;
+        $url = API_URL . '/';
+        if (!empty($this->userdata['token'])){
+            $userToken = $this->userdata['token'];
+            $result = callAPI("POST", $url, $normalizedData, $userToken );
+        }
 
-        $this->db->query($update);
-    }*/
+        return $result;
+    }
 
+    /**
+     * Metodo edita/update Grupo
+     */
+    public function updateGroup($data) {
+        $result = null;
+        $normalizedData = array();
 
-    /*public function removeCategory($id){
-        $remove = null;
+        // Not active by default
+        $normalizedData['Active'] = "";
 
-        $remove = 'DELETE FROM categories WHERE catid = ' . $id;
-        $this->db->query($remove);
+        // get data from form array and package it to send to api
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']){ //gets <input name="">
+                    case "editGroupName":
+                        $normalizedData['Name'] = $value;
+                        break;
 
-        $remove = 'DELETE FROM movies_categories WHERE catid = ' . $id;
-        $this->db->query($remove);
-    }*/
+                    case "editGroupDescription":
+                        $normalizedData['Description'] = $value;
+                        break;
+
+                    case "editGroupSecurityId":
+                        $normalizedData['SecurityId'] = $value;
+                        break;
+
+                    case "editGroupActive":
+                        $normalizedData['Active'] = "True";
+                        break;
+                }
+            }
+        }
+
+        $url = API_URL . '/' . $normalizedData['Id'];
+        if (!empty($this->userdata['token'])){
+            $userToken = $this->userdata['token'];
+            $result = callAPI("POST", $url, $normalizedData, $userToken );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Metodo delete Grupo
+     */
+    public function deleteGroup($data) {
+        $result = null;
+        $GroupId = null;
+
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']){ //gets input name=""
+                    case "deleteGroupId":
+                        $GroupId = $value;
+                        break;
+                }
+            }
+        }
+
+        $url = API_URL . '/' . $GroupId;
+
+        if (!empty($this->userdata['token'])){
+            $userToken = $this->userdata['token'];
+            $result = callAPI("DELETE", $url, '', $userToken );
+        }
+
+        return $result;
+    }
 
     /**
      * Metodo que retorna 10 categorias da BD
