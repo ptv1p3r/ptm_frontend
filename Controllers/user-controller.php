05e17da89bca32373c40ba9865499896a284b25a
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-04-14 17:37:48 +0100

    ADD function newUser to handler ajax call's

--- Controllers/user-controller.php
+++ Controllers/user-controller.php
@@ -11,24 +11,37 @@
 class UserController extends MainController
 {
     public function index() {
-
         /**
          * Carrega a página
          * "/views/home/newuser-view.php"
          */
 
         // Título da página
-        $this->title = 'New User';
+        $this->title = 'User';
 
         // Parametros da função
         $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
 
-       // $modelo = $this->load_model('user-model');
+        //$modelo = $this->load_model('user-model');
 
-       /* $moviesTopRated = $modelo->getTopRatedList(4);
-        $moviesTopDownloaded = $modelo->getTopDownloaded(4);
-        $moviesLastAdded = $modelo->getLastAdded(4);*/
-        echo('batatas');
+        /** Carrega os arquivos do view **/
+        require ABSPATH . '/views/_includes/header.php';
+        require ABSPATH . '/views/home/newuser-view.php';
+        require ABSPATH . '/views/_includes/footer.php';
+    }
+
+    public function newUser(){
+        /**
+         * Carrega ação
+         */
+
+        // Título da página
+        $this->title = 'New User';
+        echo($_POST['action']);
+
+        // Parametros da função
+        $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+
         // processa chamadas ajax
         if(isset($_POST['action']) && !empty($_POST['action'])) {
             $action = $_POST['action'];
@@ -36,30 +49,10 @@ class UserController extends MainController
             $data = $_POST['data'];
             echo($data);
             echo('tomates');
-            /*$apiResponse = json_decode($model->addNewUser($data),true); //decode to check message from api
 
-            if ($apiResponse['message'] == "POI Category added successfully"){
-                $apiResponse['code'] = 200;
-            } else {
-                $apiResponse['code'] = 400;
-            }*/
             $apiResponse = 'OK';
             $apiResponse = json_encode($apiResponse); // encode package to send
             echo $apiResponse;
-
         }
-
-
-        /** Carrega os arquivos do view **/
-
-
-        require ABSPATH . '/views/_includes/header.php';
-
-        require ABSPATH . '/views/home/newuser-view.php';
-
-        require ABSPATH . '/views/_includes/footer.php';
-
     }
-
-
 }
