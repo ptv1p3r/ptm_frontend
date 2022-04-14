838bba9116561c2539af5b54b5b7e8dfdddc98f1
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-04-14 23:30:15 +0100

    ADD function addUser

--- Models/user-model.php
+++ Models/user-model.php
@@ -4,7 +4,7 @@ class UserModel extends MainModel
 {
 
     /**
-     * O objeto da nossa conexão PDO
+     * Objet to connect PDO - DB
      *
      * @access public
      */
@@ -22,7 +22,66 @@ class UserModel extends MainModel
         $this->userdata = $this->controller->userdata;
     }
 
+    /**
+     * ADD new User
+     *
+     * @param $data
+     * @return bool|string|null
+     * @since 0.1
+     * @access public
+     */
+    public function addUser($data)
+    {
+        $result = null;
+        $normalizedData = array();
 
+        // Not active by default
+        $normalizedData['IsActive'] = "";
+        $normalizedData['IsFeatured'] = "";
 
+        // get data from form array and package it to send to api
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']) {
+                    case "addUserName":
+                        $normalizedData['Name'] = $value;
+                        break;
 
+                    case "addUserAddress":
+                        $normalizedData['Address'] = $value;
+                        break;
+
+                        //falta o resto dos campos do input
+                }
+            }
+        }
+
+        $url = API_URL . '/user/add';
+        if (!empty($this->userdata['token'])) {
+            $userToken = $this->userdata['token'];
+            $result = callAPI("POST", $url, $normalizedData, $userToken);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get country list
+     *
+     * @since 0.1
+     * @access public
+     */
+    public function getCountryList()
+    {
+        $result = null;
+
+        $url = API_URL . '/countries/list';
+
+        if (!empty($this->userdata['token'])) {
+            $userToken = $this->userdata['token'];
+            $result = callAPI("GET", $url, '', $userToken);
+        }
+
+        return json_decode($result, true);
+    }
 }
