dc0d164466a1bfd1fa232c580f9b74af4b999c97
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-04-16 12:41:51 +0100

    ADD view to terms of services and Privacy Policy

---
+++ Views/home/rights-view.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: V1p3r
+ * Date: 17/10/2018
+ * Time: 20:14
+ */
+?>
+<?php if ( ! defined('ABSPATH')) exit; ?>
+
+<!--New user register Start-->
+<section class="wf100 p80">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-18">
+                <div>
+                    <h1>Regulamento do Projeto “Adote uma Árvore”</h1>
+                </div>
+                <div>
+                    <h2>Artigo 1º</h2>
+                </div>
+                Disposições Gerais
+                O presente regulamento tem como finalidade disciplinar o programa “Adote uma Árvore”
+                dinamizado pelos alunos do curso de Direito do ISMAT – Instituto Superior Manuel
+                Teixeira Gomes, em Portimão, no âmbito da unidade curricular Direito do Urbanismo e
+                do Ambiente.
+                Artigo 2.º
+                Colaboração
+                Para além do curso de Direito, a elaboração e desenvolvimento do presente projeto tem a
+                colaboração de vários cursos lecionados no ISMAT promovendo a/uma
+                interdisciplinaridade, nomeadamente:
+                a) Engenharia Informática – Na criação da plataforma informática;
+                b) Curso de Design – No desenvolvimento e publicitação da plataforma;
+                c) Curso de Arquitetura – Contribuir para o desenvolvimento de especificidades
+                técnicas, em especial, na área do ordenamento dos solos.
+            </div>
+        </div>
+    </div>
+</section>
+<!--New user register End teste-->
