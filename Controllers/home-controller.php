196091846ce77c1cc63af88b8dedb3b90c93ffc1
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-04-18 16:43:42 +0100

    ADD right view in this controller
    DELETE login and logout function

--- Controllers/home-controller.php
+++ Controllers/home-controller.php
@@ -38,9 +38,26 @@ class HomeController extends MainController
 
     }
 
+    public function rights()
+    {
+        /**
+         * Page load
+         * "/views/home/rights-view.php"
+         */
 
+        // Title page
+        $this->title = 'User';
 
+        // Function parameters
+        $parametros = (func_num_args() >= 1) ? func_get_arg(0) : array();
 
+        //$modelo = $this->load_model('user-model');
+
+        /** load files from view **/
+        require ABSPATH . '/views/_includes/header.php';
+        require ABSPATH . '/views/home/rights-view.php';
+        require ABSPATH . '/views/_includes/footer.php';
+    }
 
 }
 
