36dc31d69e5832d6b8d1c9f19e4d659da4e452df
AndMJ <andrejoao070@gmail.com>
2022-04-21 20:39:17 +0100

    FIX functions addSecurity, editSecurity & deleteSecurity

--- Models/security-model.php
+++ Models/security-model.php
@@ -46,12 +46,12 @@ class SecurityModel extends MainModel {
     public function getSecurityById($id) {
         $result = null;
 
-        $url = API_URL . '/' . $id;
+        $url = API_URL . 'api/v1/security/view/' . $id;
 
-        if (!empty($this->userdata['token'])){
-            $userToken = $this->userdata['token'];
-            $result = callAPI("GET", $url, '', $userToken );
-        }
+        //if (!empty($this->userdata['token'])){
+            //$userToken = $this->userdata['token'];
+            $result = callAPI("GET", $url, ''/*, $userToken */);
+        //}
 
         return json_decode($result, true);
     }
@@ -63,7 +63,7 @@ class SecurityModel extends MainModel {
     public function getSecurityList() {
         $result = null;
 
-        $url = API_URL . '/';
+        $url = API_URL . 'api/v1/security/list';
 
         if (!empty($this->userdata['token'])){
             $userToken = $this->userdata['token'];
@@ -82,33 +82,102 @@ class SecurityModel extends MainModel {
         $result = null;
         $normalizedData = array();
 
-        // Not active by default
-        $normalizedData['Active'] = "";
-
         // get data from form array and package it to send to api
         foreach ($data as $dataVector) {
             foreach ($dataVector as $key => $value) {
                 switch ($dataVector['name']) { //gets <input name="">
-                    case "addSecurityName":
-                        $normalizedData['Name'] = $value;
+                    case "addSecurityHomeLogin":
+                        $normalizedData['homeLogin'] = $dataVector['value'];
                         break;
 
-                    case "addSecurityDescription":
-                        $normalizedData['Description'] = $value;
+                    case "addSecurityAdmLogin":
+                        $normalizedData['admLogin'] = $dataVector['value'];
                         break;
 
-                    case "addSecuritySecurityId":
-                        $normalizedData['SecurityId'] = $value;
+                    case "addSecurityUsersCreate":
+                        $normalizedData['usersCreate'] = $dataVector['value'];
                         break;
 
-                    case "addSecurityActive":
-                        $normalizedData['Active'] = "True";
+                    case "addSecurityUsersRead":
+                        $normalizedData['usersRead'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityUsersUpdate":
+                        $normalizedData['usersUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityUsersDelete":
+                        $normalizedData['usersDelete'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityUsersGroupsCreate":
+                        $normalizedData['usersGroupsCreate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityUsersGroupsRead":
+                        $normalizedData['usersGroupsRead'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityUsersGroupsUpdate":
+                        $normalizedData['usersGroupsUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityUsersGroupsDelete":
+                        $normalizedData['usersGroupsDelete'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesCreate":
+                        $normalizedData['treesCreate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesRead":
+                        $normalizedData['treesRead'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesUpdate":
+                        $normalizedData['treesUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesDelete":
+                        $normalizedData['treesDelete'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesTypeCreate":
+                        $normalizedData['treesTypeCreate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesTypeRead":
+                        $normalizedData['treesTypeRead'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesTypeUpdate":
+                        $normalizedData['treesTypeUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesTypeDelete":
+                        $normalizedData['treesTypeDelete'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesImagesCreate":
+                        $normalizedData['treesImagesCreate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesImagesRead":
+                        $normalizedData['treesImagesRead'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesImagesUpdate":
+                        $normalizedData['treesImagesUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "addSecurityTreesImagesDelete":
+                        $normalizedData['treesImagesDelete'] = $dataVector['value'];
                         break;
                 }
             }
         }
 
-        $url = API_URL . '/';
+        $url = API_URL . 'api/v1/security/create';
         if (!empty($this->userdata['token'])){
             $userToken = $this->userdata['token'];
             $result = callAPI("POST", $url, $normalizedData, $userToken );
@@ -124,6 +193,7 @@ class SecurityModel extends MainModel {
      */
     public function updateSecurity($data) {
         $result = null;
+        $SecurityId = null;
         $normalizedData = array();
 
         // Not active by default
@@ -133,26 +203,102 @@ class SecurityModel extends MainModel {
         foreach ($data as $dataVector) {
             foreach ($dataVector as $key => $value) {
                 switch ($dataVector['name']){ //gets <input name="">
-                    case "editSecurityName":
-                        $normalizedData['Name'] = $value;
+                    case "editSecurityId":
+                        $SecurityId = $dataVector['value'];
                         break;
 
-                    case "editSecurityDescription":
-                        $normalizedData['Description'] = $value;
+                    case "editSecurityHomeLogin":
+                        $normalizedData['homeLogin'] = $dataVector['value'];
                         break;
 
-                    case "editSecuritySecurityId":
-                        $normalizedData['SecurityId'] = $value;
+                    case "editSecurityAdmLogin":
+                        $normalizedData['admLogin'] = $dataVector['value'];
                         break;
 
-                    case "editSecurityActive":
-                        $normalizedData['Active'] = "True";
+                    case "editSecurityUsersCreate":
+                        $normalizedData['usersCreate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityUsersRead":
+                        $normalizedData['usersRead'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityUsersUpdate":
+                        $normalizedData['usersUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityUsersDelete":
+                        $normalizedData['usersDelete'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityUsersGroupsCreate":
+                        $normalizedData['usersGroupsCreate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityUsersGroupsRead":
+                        $normalizedData['usersGroupsRead'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityUsersGroupsUpdate":
+                        $normalizedData['usersGroupsUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityUsersGroupsDelete":
+                        $normalizedData['usersGroupsDelete'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesCreate":
+                        $normalizedData['treesCreate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesRead":
+                        $normalizedData['treesRead'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesUpdate":
+                        $normalizedData['treesUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesDelete":
+                        $normalizedData['treesDelete'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesTypeCreate":
+                        $normalizedData['treesTypeCreate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesTypeRead":
+                        $normalizedData['treesTypeRead'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesTypeUpdate":
+                        $normalizedData['treesTypeUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesTypeDelete":
+                        $normalizedData['treesTypeDelete'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesImagesCreate":
+                        $normalizedData['treesImagesCreate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesImagesRead":
+                        $normalizedData['treesImagesRead'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesImagesUpdate":
+                        $normalizedData['treesImagesUpdate'] = $dataVector['value'];
+                        break;
+
+                    case "editSecurityTreesImagesDelete":
+                        $normalizedData['treesImagesDelete'] = $dataVector['value'];
                         break;
                 }
             }
         }
 
-        $url = API_URL . '/' . $normalizedData['Id'];
+        $url = API_URL . 'api/v1/security/edit/' . $SecurityId;
         if (!empty($this->userdata['token'])){
             $userToken = $this->userdata['token'];
             $result = callAPI("POST", $url, $normalizedData, $userToken );
@@ -180,7 +326,7 @@ class SecurityModel extends MainModel {
             }
         }
 
-        $url = API_URL . '/' . $SecurityId;
+        $url = API_URL . 'api/v1/security/delete/' . $SecurityId;
 
         if (!empty($this->userdata['token'])){
             $userToken = $this->userdata['token'];
