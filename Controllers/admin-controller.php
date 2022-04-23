76d320107141d8573206bb2d23426dd6e15c9b70
AndMJ <andrejoao070@gmail.com>
2022-04-23 20:00:02 +0100

    FIX index() page title & load_model() param

--- Controllers/admin-controller.php
+++ Controllers/admin-controller.php
@@ -11,17 +11,17 @@ class AdminController extends MainController
 
     /**
      * Carrega a página
-     * "/views/admin/admin-login-view.php"
+     * "/views/admin/admin-dashboard-view.php"
      */
     public function index() {
 
         // Título da página
-        $this->title = 'Admin';
+        $this->title = 'Admin - Login';
 
         // Parametros da função
         $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
 
-        $modelo = $this->load_model('admin-model');
+        $modelo = $this->load_model('admin-login-model');
 
         /** Carrega os arquivos do view **/
 
@@ -31,6 +31,77 @@ class AdminController extends MainController
 
         require ABSPATH . '/views/_includes/admin-footer.php';
 
+    }
+
+    /**
+     * Login
+     * @return void
+     */
+    public function login() {
+        // Título da página
+        $this->title = 'Admin';
+
+        // Parametros da função
+        $parametros = (func_num_args() >= 1) ? func_get_arg(0) : array();
+
+        $modelo = $this->load_model('admin-login-model');
+
+        if (isset($_POST["submit"])) {
+            if (!empty($_POST['email']) && !empty($_POST['pass'])) {
+                $email = $_POST['email'];
+                $pass = $_POST['pass'];
+
+                $response = $modelo->validateUser($email, $pass);
+
+                if ($response != null) {
+                    // validar se login é valido e guardar tokens para a $_SESSION
+                    //TODO: login, fazer validaçao correta de responce da api
+                    if (boolval($response["auth"]) == true) {
+
+                        if(!isset($_SESSION)) {
+                            session_start();
+                        }
+
+                        // user passa a estar logged in e entao a ter acesso a paginas admin
+                        $this->logged_in = true;
+
+                        // Recria o ID da sessão
+                        $session_id = session_id();
+
+                        // Atualiza user
+                        $_SESSION['userdata']['email'] = $email;
+
+                        // Atualiza a senha
+                        $_SESSION['userdata']['password'] = $pass;
+
+                        // Atualiza o token
+                        $_SESSION['userdata']['accessToken'] = $response["accessToken"];
+
+                        // Atualiza o token
+                        $_SESSION['userdata']['refreshToken'] = $response["refreshToken"];
+
+                        // Atualiza o ID da sessão
+                        $_SESSION['userdata']['user_session_id'] = $session_id;
+
+                        $_POST['validation'] = "success";
+                        $this->goto_page(HOME_URL . '/admin/dashboard');
+
+                    }
+                } else {
+                    $_POST['validation'] = "failed";
+
+                    $this->index();
+                }
+
+            } else {
+                $_POST['validation'] = "failed";
+
+                $this->index();
+            }
+
+        } else {
+            $this->index();
+        }
     }
 
     /**
@@ -164,76 +235,6 @@ class AdminController extends MainController
     }
 
 
-    /**
-     * Login
-     * @return void
-     */
-    public function login() {
-        // Título da página
-        $this->title = 'Admin';
-
-        // Parametros da função
-        $parametros = (func_num_args() >= 1) ? func_get_arg(0) : array();
-
-        $modelo = $this->load_model('admin-model');
-
-        if (isset($_POST["submit"])) {
-            if (!empty($_POST['email']) && !empty($_POST['pass'])) {
-                $email = $_POST['email'];
-                $pass = $_POST['pass'];
-
-                $response = $modelo->validateUser($email, $pass);
-
-                if ($response != null) {
-                    // validar se login é valido e guardar tokens para a $_SESSION
-                    //TODO: login, fazer validaçao correta de responce da api
-                    if ($response["auth"] == true) {
-
-                        if(!isset($_SESSION)) {
-                            session_start();
-                        }
-
-                        // user passa a estar logged in e entao a ter acesso a paginas admin
-                        $this->logged_in = true;
-
-                        // Recria o ID da sessão
-                        $session_id = session_id();
-
-                        // Atualiza user
-                        $_SESSION['userdata']['email'] = $email;
-
-                        // Atualiza a senha
-                        $_SESSION['userdata']['password'] = $pass;
-
-                        // Atualiza o token
-                        $_SESSION['userdata']['token'] = $response["accessToken"];
-
-                        // Atualiza o token
-                        $_SESSION['userdata']['refreshToken'] = $response["refreshToken"];
-
-                        // Atualiza o ID da sessão
-                        $_SESSION['userdata']['user_session_id'] = $session_id;
-
-                        $_POST['validation'] = "success";
-                        $this->dashboard();
-
-                    }
-                } else {
-                    $_POST['validation'] = "failed";
-
-                    $this->index();
-                }
-
-            } else {
-                $_POST['validation'] = "failed";
-
-                $this->index();
-            }
-
-        } else {
-            $this->index();
-        }
-    }
 
 
 }
