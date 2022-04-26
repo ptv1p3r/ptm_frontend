9d533910ed04c756179100e3f99226f35699a1d9
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-04-26 23:38:06 +0100

    ADD login function
    ADD logout function

--- Controllers/home-controller.php
+++ Controllers/home-controller.php
@@ -5,6 +5,7 @@
  * Date: 17/10/2018
  * Time: 20:10
  */
+
 /**
  * home - Controller
  */
@@ -14,13 +15,14 @@ class HomeController extends MainController
     /**
      * Carrega a página "/views/home/home-view.php"
      */
-    public function index() {
+    public function index()
+    {
 
         // Título da página
         $this->title = 'Home';
 
         // Parametros da função
-        $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+        $parametros = (func_num_args() >= 1) ? func_get_arg(0) : array();
 
         /*$modelo = $this->load_model('home-model');
 
@@ -37,6 +39,7 @@ class HomeController extends MainController
         require ABSPATH . '/views/_includes/footer.php';
 
     }
+
 
     public function rights()
     {
@@ -57,6 +60,89 @@ class HomeController extends MainController
         require ABSPATH . '/views/_includes/header.php';
         require ABSPATH . '/views/home/rights-view.php';
         require ABSPATH . '/views/_includes/footer.php';
+    }
+
+    /**
+     * Login
+     * @return void
+     */
+    public function login() {
+        // Título da página
+        $this->title = 'User';
+
+        // Parametros da função
+        $parametros = (func_num_args() >= 1) ? func_get_arg(0) : array();
+
+        $modelo = $this->load_model('home-login-model');
+
+        if (isset($_POST["submit"])) {
+            if (!empty($_POST['email']) && !empty($_POST['pass'])) {
+                $email = $_POST['email'];
+                $pass = hash('sha256', $_POST['pass']);
+
+                $response = $modelo->validateUser($email, $pass);
+
+                if ($response != null) {
+                    // verifica se a autenticaçao esta correta e guarda tokens para a $_SESSION
+                    //TODO: login, fazer validaçao correta de responce da api
+                    if (boolval($response["auth"]) == true) {
+
+                        if(!isset($_SESSION)) {
+                            session_start();
+                        }
+
+                        // user passa a estar logged in e entao a ter acesso a paginas admin
+                        $this->logged_in = true;
+
+                        // Recria o ID da sessão
+                        $session_id = session_id();
+
+                        // Atualiza user
+                        $_SESSION['userdata']['email'] = $email;
+
+                        // Atualiza a senha
+                        $_SESSION['userdata']['password'] = $pass;
+
+                        // Atualiza o token
+                        $_SESSION['userdata']['accessToken'] = $response["accessToken"];
+
+                        // Atualiza o token
+                        $_SESSION['userdata']['refreshToken'] = $response["refreshToken"];
+
+                        // Atualiza o ID da sessão
+                        $_SESSION['userdata']['user_session_id'] = $session_id;
+
+                        $_POST['validation'] = "success";
+                        $_SESSION['goto_url'] = '/home';
+                        $this->goto_page(HOME_URL . '/home');
+
+                    }
+                } else {
+                    $_POST['validation'] = "failed";
+
+                    $this->index();
+                }
+
+            } else {
+                $_POST['validation'] = "failed";
+
+                $this->index();
+            }
+
+        } else {
+            $this->index();
+        }
+    }
+
+    /**
+     * Login
+     * @return void
+     */
+
+    function applogout(){
+        $_SESSION['goto_url'] = '/home';
+
+        $this->logout(true);
     }
 
 }
