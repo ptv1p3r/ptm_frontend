8cdcd45bf9be2c7f69c2cf552b73836ae7f16754
AndMJ <andrejoao070@gmail.com>
2022-04-27 02:34:27 +0100

    ADD login ajax
    ADD login alert message

--- Views/admin/admin-login-view.php
+++ Views/admin/admin-login-view.php
@@ -18,23 +18,74 @@
     <div class="row  justify-content-center align-items-center">
         <div class="col-lg-4">
             <div class="jumbotron" style="margin-top: 150px">
-                <?php if (isset($_POST['validation']) && $_POST['validation'] == "failed") { ?>
-                    <div class="alert alert-danger alert-dismissable" role="alert">
+                <?php //if (isset($_POST['validation']) && $_POST['validation'] == "failed") { ?>
+                <!--<div class="alert alert-danger alert-dismissable" role="alert">
                         <a class="panel-close close" data-dismiss="alert">×</a>
-                        <i class="fa fa-times-circle"></i> Invalid Username/Password!
-                    </div>
-                <?php } ?>
+                        <i class="fa fa-times-circle"></i> Invalid Email/Password!
+                    </div>-->
+                <?php //} ?>
 
-                <form action="<?php echo HOME_URL . '/admin/login';?>" method="post">
+                <div id="liveAlert">
+
+                </div>
+
+                <form id="doLogin">
                     <input name="email" type="email" class="form-control" placeholder="Email">
                     <br>
                     <input name="pass" type="password" class="form-control" placeholder="Password">
                     <br>
-
-                    <!-- TODO: substituir action="" por ajax para login-->
-                    <input type="submit" name="submit" class="btn btn-success form-control" value="Login"/>
+                    <button type="submit" name="submit" class="btn btn-success form-control">Login</button>
                 </form>
             </div>
         </div>
     </div>
+
 </div>
+
+<script>
+    $(document).ready(function() {
+        // ajax to login
+        $('#doLogin').submit(function (event) {
+            event.preventDefault(); //prevent default action
+
+            let formData = {
+                'action': "Login",
+                'data': $(this).serializeArray()
+            };
+
+            $.ajax({
+                url: "<?php echo HOME_URL . '/admin/login';?>",
+                dataType: "json",
+                type: 'POST',
+                data: formData,
+                success: function (data) {
+
+                    if(data == 200){
+                        alert("Logging in!", "success");
+                        location.reload();
+                    } else {
+                        alert("Invalid Email/Password!", "danger");
+                    }
+
+
+
+                }
+            });
+        });
+
+        //mensagem alerta para CRUD
+        let alertPlaceholder = document.getElementById('liveAlert')
+        function alert(message, type) {
+            let wrapper = document.createElement('div')
+            wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '</div>'
+
+            alertPlaceholder.append(wrapper)
+
+            // timeout alert message
+            setTimeout(function () {
+                $(".alert").remove()
+            }, 3000);
+        }
+
+    });
+</script>
