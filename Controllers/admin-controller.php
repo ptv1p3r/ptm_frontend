8fed5d262c36522f0cc36c01c4691193cd4be453
AndMJ <andrejoao070@gmail.com>
2022-04-29 01:28:55 +0100

    FIX users() api response validation

--- Controllers/admin-controller.php
+++ Controllers/admin-controller.php
@@ -33,73 +33,6 @@ class AdminController extends MainController
 
     }
 
-    /*public function login() {
-        // Título da página
-        $this->title = 'Admin';
-
-        // Parametros da função
-        $parametros = (func_num_args() >= 1) ? func_get_arg(0) : array();
-
-        $modelo = $this->load_model('admin-model');
-
-        if (isset($_POST["submit"])) {
-            if (!empty($_POST['user']) && !empty($_POST['pass'])) {
-                $user = $_POST['user'];
-                $pass = $_POST['pass'];
-
-                $validate = $modelo->validateUser($user, $pass);
-
-                if ($validate != null) {
-                    if ($user == $validate[0]['username'] && $pass == $validate[0]['password']) {
-
-                        if(!isset($_SESSION)) {
-                            session_start();
-                        }
-
-                        $_SESSION['sess_user'] = $user;
-
-                        $_POST['validation'] = "success";
-
-                        //$this->group(1);
-                    }
-                } else {
-                    $_POST['validation'] = "failed";
-
-                    $this->index();
-                }
-
-            } else {
-                $_POST['validation'] = "failed";
-
-                $this->index();
-            }
-
-        } else {
-            $this->index();
-        }
-    }*/
-
-    /*public function logout() {
-        // Título da página
-        $this->title = 'Admin';
-
-        // Parametros da função
-        $parametros = (func_num_args() >= 1) ? func_get_arg(0) : array();
-
-        $modelo = $this->load_model('admin-model');
-
-
-        $helper = array_keys($_SESSION);
-        foreach ($helper as $key){
-            unset($_SESSION[$key]);
-        }
-
-        //unset($_SESSION['sess_user']);
-        session_destroy();
-
-        $this->index();
-    }*/
-
     /**
      * loads /admin/groups page and handles ajax
      * @return void
@@ -198,61 +131,79 @@ class AdminController extends MainController
             $action = $_POST['action'];
             switch($action) {
                 case 'GetUser' :
-                    echo json_encode($modelo->getUserById($_POST['data']));
+                    $data = $_POST['data'];
+                    echo $apiResponse = $modelo->getUserById($data);
+                    $apiResponseBody = json_encode($apiResponse["body"]);
+
+                    echo $apiResponseBody;
                     break;
 
                 case 'AddUser' :
                     $data = $_POST['data'];
+                    $apiResponse = $modelo->addUser($data); //decode to check message from api
 
-                    $apiResponse = json_decode($modelo->addUser($data),true); //decode to check message from api
+                    // quando statusCode = 201, a response nao vem com campo mensagem
+                    // entao é criado e encoded para ser enviado
+                    if ($apiResponse['statusCode'] === 201){ // 201 created
+                        $apiResponse["body"]['message'] = "Created with success!";
 
-                // TODO rever a validaçao da response que vem da api
-                    if (!$apiResponse['created']){
-                        $apiResponse['code'] = 400;
-                    } else {
-                        $apiResponse['code'] = 200;
+                        $apiResponse = json_encode($apiResponse);// encode package to send
+                        echo $apiResponse;
+                        break;
                     }
 
-                    $apiResponse = json_encode($apiResponse); // encode package to send
-                    echo $apiResponse;
+                    // se statsCode nao for 201, entao api response ja vem com um campo mensagem
+                    // assim so precisamos de fazer encode para ser enviado
+                    $apiResponse = json_encode($apiResponse);// encode package to send
+                    echo($apiResponse);
                     break;
+
 
                 case 'UpdateUser' :
                     $data = $_POST['data'];
-                    $apiResponse = json_decode($modelo->updateUser($data),true); //decode to check message from api
+                    $apiResponse = $modelo->updateUser($data); //decode to check message from api
 
-                    if (!$apiResponse['updated']){
-                        $apiResponse['code'] = 400;
-                    } else {
-                        $apiResponse['code'] = 200;
+                    if ($apiResponse['statusCode'] === 200){ // 200 OK, successful
+                        $apiResponse["body"]['message'] = "Updated with success!";
+
+                        $apiResponse = json_encode($apiResponse);// encode package to send
+                        echo $apiResponse;
+                        break;
                     }
 
-                    $apiResponse = json_encode($apiResponse); // encode package to send
-                    echo $apiResponse;
-
+                    $apiResponse = json_encode($apiResponse);// encode package to send
+                    echo($apiResponse);
                     break;
 
                 case 'DeleteUser' :
                     $data = $_POST['data'];
-                    $apiResponse = json_decode($modelo->deleteUser($data),true); //decode to check message from api
+                    $apiResponse = $modelo->deleteUser($data); //decode to check message from api
 
-                    if (!$apiResponse['deleted']){
-                        $apiResponse['code'] = 400;
-                    } else {
-                        $apiResponse['code'] = 200;
+                    if ($apiResponse['statusCode'] === 200){ // 200 OK, successful
+                        $apiResponse["body"]['message'] = "Deleted with success!";
+
+                        $apiResponse = json_encode($apiResponse);// encode package to send
+                        echo $apiResponse;
+                        break;
                     }
 
-                    $apiResponse = json_encode($apiResponse); // encode package to send
-                    echo $apiResponse;
-
+                    $apiResponse = json_encode($apiResponse);// encode package to send
+                    echo($apiResponse);
                     break;
             }
 
         } else {
-            $this->userdata['usersList'] = $modelo->getUserList();
-            $this->userdata['countryList'] = $modelo->getCountryList();
-            /**Carrega os arquivos do view**/
+            $userList = $modelo->getUserList();
+            if ($userList["statusCode"] != 401){
+                $this->userdata['usersList'] = $userList["body"];
+            }
 
+            $countryList = $modelo->getCountryList();
+            if ($countryList["statusCode"] != 401){
+                $this->userdata['countryList'] = $countryList["body"];
+            }
+
+            /**Carrega os arquivos do view**/
             require ABSPATH . '/views/_includes/admin-header.php';
 
             require ABSPATH . '/views/admin/admin-users-view.php';
