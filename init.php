7985b4de9a042b2219fb692d211430743adf7cba
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-05-15 12:22:48 +0100

    ADD cors policy handler

--- init.php
+++ init.php
@@ -6,22 +6,48 @@
  * Time: 22:33
  */
 //ini_set("memory_limit",-1);
-// Evita acesso directo
-if ( ! defined('ABSPATH')) exit;
+// Prevents direct access
+if (!defined('ABSPATH')) exit;
 
-// Inicia a sessão
+// Logs in
 session_start();
 
-// Verifica debug
-if ( ! defined('DEBUG') || DEBUG === false ) {
+//Cors policy handler
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+    // you want to allow, and if so:
+//    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+//    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+//    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+}
 
-    // Esconde todos os erros
+//// Access-Control headers are received during OPTIONS requests
+//if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//
+//    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+//        // may also be using PUT, PATCH, HEAD etc
+//        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+//
+//    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+//        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+//
+//    exit(0);
+//}
+
+// Check debug
+if (!defined('DEBUG') || DEBUG === false) {
+
+    // Hides all errors
     error_reporting(0);
     ini_set("display_errors", 0);
 
 } else {
 
-    // Mostra todos os erros
+    // Show all errors
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 
