0d2229de0780187dace9b702b4f46489ac23b424
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-05-21 12:53:13 +0100

    FIX view container
    ADD checkbox to see the imput password
    
    FIX the button submit
    
    FIX ajax call path url(doesn't work)
    
    FIX error handler messages if password don´t match
    
    ADD function handler the visibility password

--- Views/user/registration/pass-recover-view.php
+++ Views/user/registration/pass-recover-view.php
@@ -10,72 +10,63 @@
 
 
 <div id="wrapper">
-    <!--New User Register Start-->
+    <!--Recover password start-->
     <section class="wf100 p80">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-18">
-                    <div class="myaccount-form">
-                        <div>
-                            <h3>Reponha a sua palavra passe:</h3>
-                            <!--                            <h4>É rápido e fácil.</h4>-->
-                            <br>
+            <div class="recover">
+                <div class="row">
+                    <div class="col-lg-18">
+                        <div class="myrecover-form">
+                            <div>
+                                <h3>Reponha a sua palavra passe</h3>
+                                <br>
+                            </div>
+                            <!--  Change password form-->
+                            <form id="recPass">
+                                <div class="form-group">
+                                </div>
+                                <div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Nova password</label>
+                                            <input type="password" id="pass" name="newPass" class="form-control"
+                                                   required>
+                                            <input type="checkbox" id="show-password">
+                                            <label for="show-password">Ver palavra-passe
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Confirma a nova password</label>
+                                            <input type="password" id="verify" name="confPass" class="form-control"
+                                                   required>
+                                            <input type="checkbox" id="show-password1">
+                                            <label for="show-password">Ver palavra-passe
+                                                <span id="verifyMatchPass" class="badge-warning hidden"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Image loader -->
+                                <div class="loaderOverlay lds-dual-ring hidden" id="loader">
+                                    <svg class="loader" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="50" cy="50" r="46"/>
+                                    </svg>
+                                </div>
+                                <!-- Image loader -->
+                                <div>
+                                    <div class=" form-submit">
+                                        <button type="submit" class="btn btn-success recover">Alterar</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <!--  End change password form-->
                         </div>
-
-                        <!--  Change password form-->
-
-                        <form id="recPass">
-                            <div class="form-group">
-<!--                                <input id="passEditUserId" name="passEditUserId" type="hidden" class="form-control"-->
-<!--                                       value="--><?php //echo $user["id"] ?><!--">-->
-                            </div>
-                            <div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label>Nova password</label>
-                                        <input type="password" id="pass" name="newPass" class="form-control"
-                                               required>
-                                        <span id="verifyEqualPass" class="badge-warning hidden"></span>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label>Confirma a nova password</label>
-                                        <input type="password" id="verify" name="confPass" class="form-control"
-                                               required>
-                                        <span id="verifyMatchPass" class="badge-warning hidden"></span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Image loader -->
-                            <div class="loaderOverlay lds-dual-ring hidden" id="loader" >
-                                <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                                    <circle cx="50" cy="50" r="46"/>
-                                </svg>
-                            </div>
-                            <!-- Image loader -->
-
-
-                            <div>
-                                <div class="modal-footer">
-<!--                                    <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">-->
-                                    <input type="submit" class="btn btn-info" value="Alterar">
-                                </div>
-                            </div>
-                        </form>
-
-
                     </div>
-
-                    <!-- End Change password form-->
-
                 </div>
             </div>
         </div>
-
     </section>
-    <!--New User Register End-->
+    <!--End recover password-->
 </div>
 
 
@@ -91,13 +82,10 @@
                 'data': $(this).serializeArray()
             };
 
-
-
             console.log(formData);
 
             $.ajax({
-                url: "<?php echo HOME_URL . '/register/passRecover';?>",
-                //url: "<?php //echo API_URL . 'register/passrecover';?>//",
+                url: "<?php echo HOME_URL . '/register/passRecover/' . chk_array($parametros, 0) . '/' . chk_array($parametros, 1);?>",
                 dataType: "json",
                 type: 'POST',
                 data: formData,
@@ -117,8 +105,8 @@
                                 window.location.href = "<?php echo HOME_URL . '/home';?>";
                             }
                         });
-                    } else {
-                        //mensagem de Error
+                    } else if (data.statusCode === 400) {
+                        //Error message
                         Swal.fire({
                             title: 'Error!',
                             text: data.body.message,
@@ -129,6 +117,11 @@
                                 //location.reload();
                             }
                         });
+                    } else {
+                        if (data.statusCode == 0) {
+                            $('#verifyMatchPass').text(data.body.message);
+                            $('#verifyMatchPass').show();
+                        }
                     }
                 }, complete: function () { // Set our complete callback, adding the .hidden class and hiding the spinner.
                     $('#loader').addClass('hidden')
@@ -148,6 +141,18 @@
                 },
             });
         });
+    });
+    //function handler spn messages
+    $('#updatePass').click(function () {
+        $(this).parent().find('#verifyMatchPass').hide();
+    });
+
+    <!-- Toggle password visibility -->
+    $("#show-password").change(function () {
+        $(this).prop("checked") ? $("#pass").prop("type", "text") : $("#pass").prop("type", "password");
+    });
+    $("#show-password1").change(function () {
+        $(this).prop("checked") ? $("#verify").prop("type", "text") : $("#verify").prop("type", "password");
     });
 
 
