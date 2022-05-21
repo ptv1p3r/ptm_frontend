80bdfd0039ee6b3196403bc4f92f9fde6aad2633
AndMJ <andrejoao070@gmail.com>
2022-05-21 17:25:20 +0100

    ADD tree_types model

---
+++ Models/admin-tree-types-model.php
@@ -0,0 +1,243 @@
+<?php
+
+class AdminTreeTypesModel extends MainModel {
+
+    public $db; // PDO
+
+    public function __construct($db = false, $controller = null)
+    {
+
+        $this->db = $db; // Configura o DB (PDO)
+
+        $this->controller = $controller; // Configura o controlador
+
+        $this->parametros = $this->controller->parametros; // Configura os parâmetros
+
+        $this->userdata = $this->controller->userdata;
+    }
+
+    /**
+     * Metodo que retorna lista de Users
+     * @return mixed
+     */
+    public function getUserList() {
+        $result = null;
+
+        $url = API_URL . 'api/v1/users/list';
+
+        if (!empty($_SESSION['userdata']['accessToken'])){
+            $userToken = $_SESSION['userdata']['accessToken'];
+            $result = callAPI("GET", $url, '', $userToken);
+        }
+        //trasforma toda a msg em string json para poder ser enviado
+        return json_decode(json_encode($result), true);
+    }
+
+    /** CRUD TREES **/
+    /**
+     * Metodo que retorna treetype pelo id
+     * @param $id
+     * @return mixed
+     */
+    public function getTreeTypeById($id) {
+        $result = null;
+
+        $url = API_URL . 'api/v1/treetype/view/' . $id;
+        if (!empty($_SESSION['userdata']['accessToken'])){
+            $userToken = $_SESSION['userdata']['accessToken'];
+            $result = callAPI("GET", $url, '', $userToken);
+        }
+        //trasforma toda a msg em string json para poder ser enviado
+        return json_decode(json_encode($result), true);
+    }
+
+    /**
+     * Metodo que retorna lista de treetype
+     * @return mixed
+     */
+    public function getTreeTypeList()
+    {
+        $result = null;
+
+        $url = API_URL . 'api/v1/treetype/list';
+        if (!empty($_SESSION['userdata']['accessToken'])){
+            $userToken = $_SESSION['userdata']['accessToken'];
+            $result = callAPI("GET", $url, '', $userToken);
+        }
+        //trasforma toda a msg em string json para poder ser enviado
+        return json_decode(json_encode($result), true);
+    }
+
+    /**
+     * Metodo adiciona TreeType
+     * @param $data
+     * @return array|null
+     */
+    public function addTreeType($data) {
+        $result = null;
+        $normalizedData = array();
+
+        // Not active by default
+        $normalizedData['active'] = "";
+
+        // get data from form array and package it to send to api
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']) { //gets <input name="">
+                    case "addTreeTypeName":
+                        $normalizedData['name'] = $dataVector['value'];
+                        break;
+
+                    case "addTreeTypeDescription":
+                        $normalizedData['description'] = $dataVector['value'];
+                        break;
+                }
+
+                if ($dataVector['name'] == "addTreeTypeActive"){
+                    $normalizedData['active'] = "1";
+                } else {
+                    $normalizedData['active'] = "0";
+                }
+            }
+        }
+
+        $url = API_URL . 'api/v1/treetype/create';
+        if (!empty($_SESSION['userdata']['accessToken'])){
+            $userToken = $_SESSION['userdata']['accessToken'];
+            $result = callAPI("POST", $url, $normalizedData, $userToken);
+        }
+
+        //trasforma toda a msg em string json para poder ser enviado
+        return json_decode(json_encode($result), true);
+    }
+
+    /**
+     * Metodo edita/update TreeType
+     * @param $data
+     * @return mixed
+     */
+    public function updateTreeType($data) {
+        $result = null;
+        $TreeTypeId = null;
+        $normalizedData = array();
+
+        // Not active by default
+        $normalizedData['active'] = "";
+
+        // get data from form array and package it to send to api
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']){ //gets <input name="">
+                    case "editTreeTypeId":
+                        $TreeTypeId = $dataVector['value'];
+                        break;
+
+                    case "editTreeTypeName":
+                        $normalizedData['name'] = $dataVector['value'];
+                        break;
+
+                    case "editTreeTypeDescription":
+                        $normalizedData['description'] = $dataVector['value'];
+                        break;
+                }
+
+                if ($dataVector['name'] == "editTreeTypeActive"){
+                    $normalizedData['active'] = "1";
+                } else {
+                    $normalizedData['active'] = "0";
+                }
+            }
+        }
+
+        $url = API_URL . 'api/v1/treetype/edit/' . $TreeTypeId;
+        if (!empty($_SESSION['userdata']['accessToken'])){
+            $userToken = $_SESSION['userdata']['accessToken'];
+            $result = callAPI("PUT", $url, $normalizedData, $userToken);
+        }
+
+        //trasforma toda a msg em string json para poder ser enviado
+        return json_decode(json_encode($result), true);
+    }
+
+    /**
+     * Metodo edita/update Trees com patch
+     * @param $data
+     * @return mixed
+     */
+    /*public function updateTree($data) {
+        $result = null;
+        $GroupId = null;
+        $normalizedData = array();
+
+        // Not active by default
+        $normalizedData['active'] = "";
+
+        // get data from form array and package it to send to api
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']){ //gets <input name="">
+                    case "editGroupId":
+                        $GroupId = $dataVector['value'];
+                        break;
+
+                    case "editGroupName":
+                        $normalizedData['name'] = $dataVector['value'];
+                        break;
+
+                    case "editGroupDescription":
+                        $normalizedData['description'] = $dataVector['value'];
+                        break;
+
+                    case "editGroupSecurityId":
+                        $normalizedData['securityId'] = $dataVector['value'];
+                        break;
+                }
+
+                if ($dataVector['name'] == "editGroupActive"){
+                    $normalizedData['active'] = "1";
+                } else {
+                    $normalizedData['active'] = "0";
+                }
+            }
+        }
+
+        $url = API_URL . 'api/v1/groups/edit/' . $GroupId;
+        if (!empty($_SESSION['userdata']['accessToken'])){
+            $userToken = $_SESSION['userdata']['accessToken'];
+            $result = callAPI("PATCH", $url, $normalizedData, $userToken);
+        }
+
+        //trasforma toda a msg em string json para poder ser enviado
+        return json_decode(json_encode($result), true);
+    }*/
+
+    /**
+     * Metodo delete TreeType
+     * @param $data
+     * @return mixed
+     */
+    public function deleteTreeType($data) {
+        $result = null;
+        $TreeTypeId = null;
+
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']){ //gets input name=""
+                    case "deleteTreeTypeId":
+                        $TreeTypeId = $dataVector['value'];
+                        break;
+                }
+            }
+        }
+
+        $url = API_URL . 'api/v1/treetype/delete/' . $TreeTypeId;
+        if (!empty($_SESSION['userdata']['accessToken'])){
+            $userToken = $_SESSION['userdata']['accessToken'];
+            $result = callAPI("DELETE", $url, '', $userToken);
+        }
+
+        //trasforma toda a msg em string json para poder ser enviado
+        return json_decode(json_encode($result), true);
+    }
+
+}
