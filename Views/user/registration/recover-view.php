7a9690ed8be2d44a1725de935894ad4bc3380561
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-05-21 12:54:03 +0100

    FIX view container
    FIX color swal confirm button ok

--- Views/user/registration/recover-view.php
+++ Views/user/registration/recover-view.php
@@ -10,43 +10,47 @@
 
 
 <div id="wrapper">
-    <!--New User Register Start-->
+    <!--Recover password Start-->
     <section class="wf100 p80">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-18">
-                    <div class="myaccount-form">
-                        <div>
-                            <h3>Recupere a sua conta</h3>
-<!--                            <h4>É rápido e fácil.</h4>-->
-                            <br>
-                        </div>
-                        <!--Form Init-->
-                        <form id="recover">
-                            <ul class="row">
-
-                                <li class="col-md-12">
-                                    <div class="form-group input-group">
-                                        <input type="email" name="userEmail" placeholder="Email" class="form-control"
-                                               required>
+            <div class="recover">
+                <div class="row">
+                    <div class="col-lg-18">
+                        <div class="myrecover-form">
+                            <div>
+                                <h3>Recupere a sua conta</h3>
+                                <h5>Insira o seu email para recuperar a sua conta.</h5>
+                                <br>
+                            </div>
+                            <!--Email form request-->
+                            <form id="recover">
+                                <div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+<!--                                            <label>Email</label>-->
+                                            <input type="email" name="userEmail" placeholder="Email"
+                                                   class="form-control"
+                                                   required>
+                                        </div>
                                     </div>
-                                </li>
-
-
+                                </div>
                                 <!-- Image loader -->
-                                <div class="loaderOverlay lds-dual-ring hidden" id="loader" >
-                                    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                                <div class="loaderOverlay lds-dual-ring hidden" id="loader">
+                                    <svg class="loader" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                                         <circle cx="50" cy="50" r="46"/>
                                     </svg>
                                 </div>
-                                <!-- Image loader -->
-
-                                <li class="col-md-12">
-                                    <button type="submit" class="register">Recuperar</button>
-                                </li>
-                            </ul>
-                        </form>
-                        <!--Form End-->
+                                <!-- End Image loader -->
+                                <div>
+                                    <div class=" form-submit">
+                                        <!--                                    <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">-->
+                                        <!--                                        <input type="submit" class="recover" value="Recuperar">-->
+                                        <button type="submit" class="btn btn-success recover">Recuperar</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <!--End email form request-->
+                        </div>
                     </div>
                 </div>
             </div>
@@ -54,7 +58,6 @@
     </section>
     <!--New User Register End-->
 </div>
-
 
 <!--Script's section-->
 <script>
@@ -83,6 +86,7 @@
                             text: data.body.message,
                             icon: 'success',
                             showConfirmButton: true,
+                            confirmButtonColor: '#66bb6a',
                             // timer: 3000,
                             didClose: () => {
                                 window.location.href = "<?php echo HOME_URL . '/home';?>";
@@ -120,7 +124,5 @@
             });
         });
     });
-
-
 </script>
 <!--Script's end section-->
