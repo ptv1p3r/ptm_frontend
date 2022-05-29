6a9c342848feb804f2a49560d488e1a83d872230
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-05-29 15:44:04 +0100

    ADD model adoption trees

---
+++ Models/adoption-model.php
@@ -0,0 +1,56 @@
+<?php
+
+class AdoptionModel extends MainModel
+{
+
+    /**
+     * Objet to connect PDO - DB
+     *
+     * @access public
+     */
+    public $db;
+
+    public function __construct($db = false, $controller = null)
+    {
+
+//        $this->db = $db; // Configura o DB (PDO)
+
+        $this->controller = $controller; // Configura o controlador
+
+        $this->parametros = $this->controller->parametros; // Configura os parâmetros
+
+        $this->userdata = $this->controller->userdata;
+    }
+
+    /**
+     * Get free to adoption trees list
+     *
+     * @since 0.1
+     * @access public
+     */
+    public function getAdoptTreesList()
+    {
+        $result = null;
+
+        $url = API_URL . 'api/v1/trees/transaction/list';
+        $result = callAPI("GET", $url, ''/*, $userToken*/);
+        return json_decode(json_encode($result), true);
+    }
+
+    /**
+     * Get gender list
+     *
+     * @since 0.1
+     * @access public
+     */
+    public function getGenderList()
+    {
+        $result = null;
+
+        $url = API_URL . 'api/v1/genders/list';
+        $result = callAPI("GET", $url, '');
+        return json_decode(json_encode($result), true);
+    }
+
+
+}
