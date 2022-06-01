39445cb9b8191f67daf1c054d34b7c50e188fda1
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-06-01 22:53:27 +0100

    ADD view to user tree by ID

---
+++ Views/user/profile/user-trees-view.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: V1p3r
+ * Date: 17/10/2018
+ * Time: 20:14
+ */
+?>
+<?php if ( ! defined('ABSPATH')) exit; ?>
+
+<!--New user register Start-->
+<section class="wf100 p80">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-18">
+                <div>
+                    <h1>A minha árvore</h1>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!--New user register End teste-->
