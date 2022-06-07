d2acfaab71380536763618610b6bb35c011b3181
AndMJ <andrejoao070@gmail.com>
2022-06-08 00:04:36 +0100

    ADD model messages

---
+++ Models/admin-messages-model.php
@@ -0,0 +1,19 @@
+<?php
+
+class AdminMessagesModel extends MainModel {
+
+    public $db; // PDO
+
+    public function __construct($db = false, $controller = null)
+    {
+
+        $this->db = $db; // Configura o DB (PDO)
+
+        $this->controller = $controller; // Configura o controlador
+
+        $this->parametros = $this->controller->parametros; // Configura os parâmetros
+
+        $this->userdata = $this->controller->userdata;
+    }
+
+}
