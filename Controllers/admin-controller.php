6ec23fc13bcab61596400b5f1c282635e7f1c1cc
AndMJ <andrejoao070@gmail.com>
2022-06-08 00:04:20 +0100

    ADD function messages()

--- Controllers/admin-controller.php
+++ Controllers/admin-controller.php
@@ -2458,6 +2458,196 @@ class AdminController extends MainController
 
     /**
      * Carrega a página
+     * "/views/admin/admin-messages-view.php"
+     * @return void
+     */
+    public function messages(){
+        // Título da página
+        $this->title = 'Admin - Mensagens';
+
+        // Permissoes da pagina
+        $this->permission_required = array('admLogin'/*,'userGroupsRead'*/);
+
+        // Estado da sidebar
+        // se ja existir uma active tab
+        /*if (isset($_SESSION["sidebar"]["active_tab"])) {
+            //remove a currently active
+            unset($_SESSION["sidebar"]["active_tab"]);
+            //e dá set a uma nova active tab
+            $_SESSION["sidebar"]["active_tab"]["messages"] = true;
+        }*/
+
+        // Parametros da função
+        $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+
+        // obriga o login para aceder à pagina
+        if ( ! $this->logged_in) {
+
+            // Se não; garante o logout
+            $this->logout();
+
+            // Redireciona para a página de login
+            $this->goto_login();
+
+            // Garante que o script não vai passar daqui
+            return;
+        }
+
+        //Verifica se o usuário tem a permissão para acessar essa página
+        if (!$this->check_permissions($this->permission_required, $_SESSION["userdata"]['user_permissions'])) {
+
+            // Exibe uma mensagem
+            echo 'Você não tem permissões para acessar essa página.';
+
+            // Finaliza aqui
+            return;
+        }
+
+        $modelo = $this->load_model('admin-messages-model');
+
+        // processa chamadas ajax
+        if(isset($_POST['action']) && !empty($_POST['action'])) {
+            $action = $_POST['action'];
+            /*switch($action) {
+                case 'GetGroup' :
+                    $data = $_POST['data'];
+                    $apiResponse = $modelo->getGroupById($data);
+                    $apiResponseBody = array();
+
+                    if ($apiResponse['statusCode'] === 401) { // 401, unauthorized
+                        //faz o refresh do accessToken
+                        $this->userTokenRefresh();
+
+                        $apiResponse = $modelo->getGroupById($data);
+                    }
+
+                    if ($apiResponse['statusCode'] === 200) { // 200 success
+                        $apiResponseBody = json_encode($apiResponse["body"]);
+                    }
+
+                    echo $apiResponseBody;
+                    break;
+
+                case 'AddGroup' :
+                    $this->permission_required = array('userGroupsCreate');
+
+                    //Verifica se o user tem a permissão para realizar operaçao
+                    if(!$this->check_permissions($this->permission_required, $_SESSION["userdata"]['user_permissions'])){
+                        $apiResponse["body"]['message'] = "You have no permission!";
+
+                        echo json_encode($apiResponse);
+                        break;
+                    }
+
+                    $data = $_POST['data'];
+                    $apiResponse = $modelo->addGroup($data); //decode to check message from api
+
+                    if ($apiResponse['statusCode'] === 401){ // 401, unauthorized
+                        //faz o refresh do accessToken
+                        $this->userTokenRefresh();
+
+                        $apiResponse = $modelo->addGroup($data); //decode to check message from api
+                        $apiResponse["body"]['message'] = "Created with success!";
+                    }
+
+                    // quando statusCode = 201, a response nao vem com campo mensagem
+                    // entao é criado e encoded para ser enviado
+                    if ($apiResponse['statusCode'] === 201){ // 201 created
+                        $apiResponse["body"]['message'] = "Created with success!";
+                    }
+
+                    // se statsCode nao for 201, entao api response ja vem com um campo mensagem
+                    // assim so precisamos de fazer encode para ser enviado
+                    $apiResponse = json_encode($apiResponse);// encode package to send
+                    echo $apiResponse;
+                    break;
+
+                case 'UpdateGroup' :
+                    $this->permission_required = array('userGroupsUpdate');
+
+                    //Verifica se o user tem a permissão para realizar operaçao
+                    if(!$this->check_permissions($this->permission_required, $_SESSION["userdata"]['user_permissions'])){
+                        $apiResponse["body"]['message'] = "You have no permission!";
+
+                        echo json_encode($apiResponse);
+                        break;
+                    }
+
+                    $data = $_POST['data'];
+                    $apiResponse = $modelo->updateGroup($data); //decode to check message from api
+
+                    if ($apiResponse['statusCode'] === 401){ // 401, unauthorized
+                        //faz o refresh do accessToken
+                        $this->userTokenRefresh();
+
+                        $apiResponse = $modelo->updateGroup($data); //decode to check message from api
+                        $apiResponse["body"]['message'] = "Updated with success!";
+                    }
+
+                    if ($apiResponse['statusCode'] === 200){ // 200 OK, successful
+                        $apiResponse["body"]['message'] = "Updated with success!";
+                    }
+
+                    $apiResponse = json_encode($apiResponse);// encode package to send
+                    echo $apiResponse;
+                    break;
+
+                case 'DeleteGroup' :
+                    $this->permission_required = array('userGroupsDelete');
+
+                    //Verifica se o user tem a permissão para realizar operaçao
+                    if(!$this->check_permissions($this->permission_required, $_SESSION["userdata"]['user_permissions'])){
+                        $apiResponse["body"]['message'] = "You have no permission!";
+
+                        echo json_encode($apiResponse);
+                        break;
+                    }
+
+                    $data = $_POST['data'];
+                    $apiResponse = $modelo->deleteGroup($data); //decode to check message from api
+
+                    if ($apiResponse['statusCode'] === 401){ // 401, unauthorized
+                        //faz o refresh do accessToken
+                        $this->userTokenRefresh();
+
+                        $apiResponse = $modelo->deleteGroup($data); //decode to check message from api
+                        $apiResponse["body"]['message'] = "Deleted with success!";
+                    }
+
+                    if ($apiResponse['statusCode'] === 200){ // 200 OK, successful
+                        $apiResponse["body"]['message'] = "Deleted with success!";
+                    }
+
+                    $apiResponse = json_encode($apiResponse);// encode package to send
+                    echo $apiResponse;
+                    break;
+            }*/
+
+        } else {
+            //get group list
+            /*$groupsList = $modelo->getGroupList();
+            if ($groupsList["statusCode"] === 200){
+                $this->userdata['groupsList'] = $groupsList["body"]["groups"];
+            }
+            if ($groupsList["statusCode"] === 401){
+                //faz o refresh do accessToken
+                $this->userTokenRefresh();
+
+                $groupsList = $modelo->getGroupList();
+                $this->userdata['groupsList'] = $groupsList["body"]["groups"];
+            }*/
+
+            /**Carrega os arquivos do view**/
+            require ABSPATH . '/views/_includes/admin-header.php';
+
+            require ABSPATH . '/views/admin/admin-messages-view.php';
+
+            require ABSPATH . '/views/_includes/admin-footer.php';
+        }
+    }
+
+    /**
+     * Carrega a página
      * "/views/admin/admin-settings-view.php"
      */
     public function settings() {
@@ -2512,7 +2702,6 @@ class AdminController extends MainController
         require ABSPATH . '/views/_includes/admin-footer.php';
 
     }
-
 
     /**
      * Metodo para logout
