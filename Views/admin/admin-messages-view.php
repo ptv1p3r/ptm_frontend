f5a92da978a8a2bf3242799c1d1b558fc2773d85
AndMJ <andrejoao070@gmail.com>
2022-06-10 18:28:35 +0100

    ADD mail inbox

--- Views/admin/admin-messages-view.php
+++ Views/admin/admin-messages-view.php
@@ -18,6 +18,167 @@
 
                     <div class="col-xl-12 col-md-12">
 
+                        <div class="container">
+                            <div class="row">
+                                <!-- BEGIN INBOX -->
+                                <div class="col-md-12">
+                                    <div class="grid email">
+                                        <div class="grid-body">
+                                            <div class="row">
+                                                <!-- BEGIN INBOX MENU -->
+                                                <div class="col-md-3">
+                                                    <h2 class="grid-title"><i class="fa fa-inbox"></i> Inbox</h2>
+                                                    <a class="btn btn-block btn-primary" data-bs-toggle="modal" data-bs-target="#composeModal"><i class="fa fa-pencil"></i>&nbsp;&nbsp;NEW MESSAGE</a>
+
+                                                    <hr>
+
+                                                    <div>
+                                                        <ul class="nav flex-column nav-pills nav-stacked">
+                                                            <li class="nav-item header">Folders</li>
+                                                            <li class="nav-item active"><a class="nav-link" href="#"><i class="fa fa-inbox"></i> Inbox (14)</a></li>
+                                                            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-star"></i> Starred</a></li>
+                                                            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-bookmark"></i> Important</a></li>
+                                                            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-mail-forward"></i> Sent</a></li>
+                                                            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-edit"></i> Drafts</a></li>
+                                                            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-folder"></i> Spam</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <!-- END INBOX MENU -->
+
+                                                <!-- BEGIN INBOX CONTENT -->
+                                                <div class="col-md-9">
+                                                    <div class="row">
+                                                        <div class="col-sm-6">
+                                                            <label style="margin-right: 8px;" class="">
+                                                                <div class="icheckbox_square-blue" style="position: relative;"><input type="checkbox" id="check-all" class="icheck" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"><ins class="iCheck-helper" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"></ins></div>
+                                                            </label>
+                                                            <div class="btn-group">
+                                                                <button type="button" class="btn btn-default dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                                                    Action <span class="caret"></span>
+                                                                </button>
+                                                                <ul class="dropdown-menu" role="menu">
+                                                                    <li><a class="dropdown-item" href="#">Mark as read</a></li>
+                                                                    <li><a class="dropdown-item" href="#">Mark as unread</a></li>
+                                                                    <li><a class="dropdown-item" href="#">Mark as important</a></li>
+                                                                    <li><hr class="dropdown-divider"></li>
+                                                                    <li><a class="dropdown-item" href="#">Report spam</a></li>
+                                                                    <li><a class="dropdown-item" href="#">Delete</a></li>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="col-md-6 search-form">
+                                                            <form action="#" class="text-right">
+                                                                <div class="input-group">
+                                                                    <input type="text" class="form-control input-sm" placeholder="Search">
+                                                                    <span class="input-group-btn">
+                                                                        <button type="submit" name="search" class="btn_ btn-primary btn-sm search"><i class="fa fa-search"></i></button>
+                                                                    </span>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="padding"></div>
+
+                                                    <div class="table-responsive">
+                                                        <table class="table">
+                                                            <tbody>
+                                                                <tr class="read">
+                                                                    <td class="action"><input type="checkbox" /></td>
+                                                                    <td class="action"><i class="fa fa-star-o"></i></td>
+                                                                    <td class="action"><i class="fa fa-bookmark-o"></i></td>
+                                                                    <td class="name"><a href="#">Larry Gardner</a></td>
+                                                                    <td class="subject"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed </a></td>
+                                                                    <td class="time">08:30 PM</td>
+                                                                </tr>
+                                                                <tr class="read">
+                                                                    <td class="action"><div class="icheckbox_square-blue" st<td class="action"><input type="checkbox" /></td>
+                                                                    <td class="action"><i class="fa fa-star"></i></td>
+                                                                    <td class="action"><i class="fa fa-bookmark"></i></td>
+                                                                    <td class="name"><a href="#">Larry Gardner</a></td>
+                                                                    <td class="subject"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed </a></td>
+                                                                    <td class="time">08:30 PM</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td class="action"><input type="checkbox" /></td>
+                                                                    <td class="action"><i class="fa fa-star-o"></i></td>
+                                                                    <td class="action"><i class="fa fa-bookmark-o"></i></td>
+                                                                    <td class="name"><a href="#">Larry Gardner</a></td>
+                                                                    <td class="subject"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed </a></td>
+                                                                    <td class="time">08:30 PM</td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+
+                                                    <!--<ul class="pagination">
+                                                        <li class="disabled"><a href="#">«</a></li>
+                                                        <li class="active"><a href="#">1</a></li>
+                                                        <li><a href="#">2</a></li>
+                                                        <li><a href="#">3</a></li>
+                                                        <li><a href="#">4</a></li>
+                                                        <li><a href="#">5</a></li>
+                                                        <li><a href="#">»</a></li>
+                                                    </ul>-->
+                                                </div>
+                                                <!-- END INBOX CONTENT -->
+
+                                                <!-- BEGIN COMPOSE MESSAGE -->
+                                                <div id="composeModal" class="modal fade" tabindex="-1" aria-labelledby="composeModal-Label" aria-hidden="true">
+                                                    <div class="modal-wrapper">
+                                                        <div class="modal-dialog">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header bg-blue">
+                                                                    <h4 class="modal-title"><i class="fa fa-envelope"></i> Compose New Message</h4>
+                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                </div>
+                                                                <form method="post">
+                                                                    <div class="modal-body">
+                                                                        <div class="form-group">
+                                                                            <label>Mail to</label>
+                                                                            <input name="to" type="email" class="form-control">
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label>CC</label>
+                                                                            <input name="cc" type="email" class="form-control">
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label>BCC</label>
+                                                                            <input name="bcc" type="email" class="form-control">
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label>Subject</label>
+                                                                            <input name="subject" type="email" class="form-control">
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label>Message</label>
+                                                                            <textarea name="message" id="email_message" class="form-control" style="height: 120px;"></textarea>
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label>Attachment</label>
+                                                                            <input type="file" name="attachment">
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                        <button type="button" class="btn btn-default" data-bs-dismiss="modal"><i class="fa fa-times"></i> Discard</button>
+                                                                        <button type="submit" class="btn btn-primary pull-right"><i class="fa fa-envelope"></i> Send Message</button>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- END COMPOSE MESSAGE -->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- END INBOX -->
+                            </div>
+                        </div>
+
                     </div>
 
                 </div>
