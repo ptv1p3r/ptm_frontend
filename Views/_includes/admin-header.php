af16ad5dbbdd0ec5ea370530a654147c2543a104
AndMJ <andrejoao070@gmail.com>
2022-06-29 20:36:55 +0100

    FIX link title

--- Views/_includes/admin-header.php
+++ Views/_includes/admin-header.php
@@ -68,7 +68,7 @@
             <span class="fs-7">Project Tree Mng.</span>
         </a>
         <!-- Sidebar Toggle-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!" title="Abrir/Fechar barra lateral"><i class="fas fa-bars"></i></button>
 
         <!-- Navbar-->
         <ul class="navbar-nav ms-auto me-3 me-lg-4">
