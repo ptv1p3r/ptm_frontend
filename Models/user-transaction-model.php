576021bd5d927161d60e03f35f25fc09e71acd25
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-06-30 00:04:29 +0100

    ADD function to reiceive transactions

--- Models/user-transaction-model.php
+++ Models/user-transaction-model.php
@@ -41,6 +41,66 @@ class UserTransactionModel extends MainModel
         return json_decode(json_encode($result), true);
     }
 
+    /**
+     * Post tree transactions
+     * @param $data
+     * @since 0.1
+     * @access public
+     */
+    public function makeTransaction($data)
+    {
+        $result = null;
+        $normalizedData = array();
+
+        //Manually injected user group data
+//        $normalizedData['groupId'] = 1;
+
+        // get data from form array and package it to send to api
+        foreach ($data as $dataVector) {
+            foreach ($dataVector as $key => $value) {
+                switch ($dataVector['name']) {
+                    case "cardNumb":
+                        $normalizedData['name'] = $dataVector['value'];
+                        break;
+
+                    case "cardExp":
+                        $normalizedData['entity'] = strlen($dataVector['value']) > 0 ? $dataVector['value'] : NULL;
+                        break;
+
+                    case "cvcpwd":
+                        $normalizedData['email'] = $dataVector['value'];
+                        break;
+
+                    case "adoptionVal":
+                        $normalizedData['password'] = $dataVector['value'];
+                        break;
+
+                    case "treeSelected":
+                        $normalizedData['dateBirth'] = $dataVector['value'];
+                        break;
+
+                    case "addUserAddress":
+                        $normalizedData['address'] = $dataVector['value'];
+                        break;
+
+
+                    case "addUserGender":
+                        $normalizedData['genderId'] = (int)$dataVector['value'];
+                        break;
+
+                }
+            }
+        }
+
+        $url = API_URL . 'api/v1/transaction/create';
+
+        if (!empty($_SESSION['userdata']['accessToken'])) {
+            $userToken = $_SESSION['userdata']['accessToken'];
+            $result = callAPI("GET", $url, '', $userToken);
+        }
+        return json_decode(json_encode($result), true);
+    }
+
 
 
 
