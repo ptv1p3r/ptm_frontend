7f73552990049da0fbd250f24c742769df41e8c2
Helder Oliveira <65983803+lireupt@users.noreply.github.com>
2022-07-13 00:32:45 +0100

    ADD tree public view

---
+++ Views/user/home/trees-view.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: V1p3r
+ * Date: 17/10/2018
+ * Time: 20:14
+ */
+?>
+<?php if (!defined('ABSPATH')) exit; ?>
+
+<!-- Image loader -->
+<div class="loaderOverlay lds-dual-ring hidden" id="loader">
+    <svg class="loader" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="46"/>
+    </svg>
+</div>
+<!-- Image loader -->
+
+
